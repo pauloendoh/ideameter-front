d516590043953060ddec71cda3e8080110ec5561
Paulo Endoh <paulo.endoh@gmail.com>
2022-05-01 10:02:34 -0300

    First push

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Container from "@mui/material/Container";
+import type { NextPage } from "next";
+import * as React from "react";
+
+const Home: NextPage = () => {
+  return <Container maxWidth="lg">Bazzoingers</Container>;
+};
+
+export default Home;
---
+++ src/theme.ts
@@ -0,0 +1,65 @@
+import { createTheme } from "@mui/material/styles";
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#1E1E1E",
+      paper: "#2B2B2B",
+    },
+    primary: { main: "#3DAC8D", contrastText: "#fff" },
+    secondary: { main: "#C862AC" },
+    info: { main: "#4E4E4E" },
+  },
+  typography: {
+    fontFamily: `'Noto Sans', sans-serif;`, // imported at global.css
+    fontSize: 13,
+    button: {
+      textTransform: "none",
+      fontWeight: 500,
+    },
+  },
+
+  components: {
+    MuiDialog: {
+      styleOverrides: {
+        paper: {
+          background: "#2B2B2B",
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          background: "#1E1E1E",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          boxShadow: "none",
+        },
+      },
+    },
+    MuiToolbar: {
+      styleOverrides: {
+        root: {
+          paddingLeft: 32,
+          paddingRight: 32,
+        },
+      },
+    },
+    MuiTooltip: {
+      styleOverrides: {
+        arrow: { color: "#292929" },
+        tooltip: {
+          backgroundColor: "#292929",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
