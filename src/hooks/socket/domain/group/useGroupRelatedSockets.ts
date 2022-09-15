36626da8e411a624df20a8abab21f09f622faf77
Paulo Endoh <paulo.endoh@gmail.com>
2022-09-15 00:53:55 -0300

    trying to fix enter-group socket io

--- src/hooks/socket/domain/group/useGroupRelatedSockets.ts
+++ src/hooks/socket/domain/group/useGroupRelatedSockets.ts
@@ -3,23 +3,19 @@ import upsert from "@/utils/array/upsert";
 import queryKeys from "@/utils/queryKeys";
 import { useEffect } from "react";
 import { useQueryClient } from "react-query";
+import { useSocketEvent } from "socket.io-react-hook";
 import { useMySocketEvent } from "../../useMySocketEvent";
+import { useGroupSocket } from "./useGroupSocket";
 
 export const useGroupRelatedSockets = (groupId: string | undefined) => {
-  const { sendMessage: sendEnterGroupMessage } = useMySocketEvent<string>(
+  const { socket } = useGroupSocket(groupId);
+  const { sendMessage: sendEnterGroupMessage } = useSocketEvent<string>(
+    socket,
     "enter-group"
   );
 
-  // first render
   useEffect(() => {
-    console.log("first render: " + groupId);
-    setTimeout(() => {
-      if (groupId) sendEnterGroupMessage(groupId);
-    }, 100);
-  }, []);
-
-  useEffect(() => {
-    console.log("second render: " + groupId);
+    console.log({ groupId, sendEnterGroupMessage });
     if (groupId) sendEnterGroupMessage(groupId);
   }, [groupId]);
 
