7bc2194edaeadbefbc4cb6cd2d6576f53eb982c4
Paulo Endoh <paulo.endoh@gmail.com>
2022-09-15 00:55:19 -0300

    sending only when socket is connected

--- src/hooks/socket/domain/group/useGroupRelatedSockets.ts
+++ src/hooks/socket/domain/group/useGroupRelatedSockets.ts
@@ -3,21 +3,17 @@ import upsert from "@/utils/array/upsert";
 import queryKeys from "@/utils/queryKeys";
 import { useEffect } from "react";
 import { useQueryClient } from "react-query";
-import { useSocketEvent } from "socket.io-react-hook";
 import { useMySocketEvent } from "../../useMySocketEvent";
-import { useGroupSocket } from "./useGroupSocket";
 
 export const useGroupRelatedSockets = (groupId: string | undefined) => {
-  const { socket } = useGroupSocket(groupId);
-  const { sendMessage: sendEnterGroupMessage } = useSocketEvent<string>(
-    socket,
-    "enter-group"
-  );
+  const { sendMessage: sendEnterGroupMessage, socket } = useMySocketEvent<
+    string
+  >("enter-group");
 
   useEffect(() => {
     console.log({ groupId, sendEnterGroupMessage });
-    if (groupId) sendEnterGroupMessage(groupId);
-  }, [groupId]);
+    if (groupId && socket.connected) sendEnterGroupMessage(groupId);
+  }, [groupId, socket.connected]);
 
   const { lastMessage } = useMySocketEvent<{ idea: IdeaDto; groupId: string }>(
     "saveIdea"
