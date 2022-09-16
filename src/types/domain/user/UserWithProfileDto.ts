400b67bf9f048b596882de0640f98bd279ec0435
Paulo Endoh <paulo.endoh@gmail.com>
2022-09-16 20:58:52 -0300

    removing file

--- src/types/domain/user/UserWithProfileDto.ts
+++
@@ -1,8 +0,0 @@
-import { ProfileDto } from "../profile/ProfileDto";
-
-export default interface UserWithProfileDto {
-  id: string;
-  email: string;
-  username: string;
-  profile: ProfileDto | null;
-}
