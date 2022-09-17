018b345c511486734bb13c18b792984c56af655d
Paulo Endoh <paulo.endoh@gmail.com>
2022-09-17 14:36:51 -0300

    sort by created / updated at

--- src/components/GroupPage/GroupTabContent/IdeaRatingsTable/IdeaRatingsTable.tsx
+++ src/components/GroupPage/GroupTabContent/IdeaRatingsTable/IdeaRatingsTable.tsx
@@ -1,21 +1,21 @@
-import { IdeaRating } from "@/hooks/react-query/domain/group/useIdeaRatingsQueryUtils";
-import useGroupFilterStore from "@/hooks/zustand/domain/auth/group/useGroupFilterStore";
-import useIdeaSortStore from "@/hooks/zustand/domain/auth/group/useIdeaSortStore";
-import useAuthStore from "@/hooks/zustand/domain/auth/useAuthStore";
-import { TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
-import { useMemo } from "react";
-import S from "./IdeaTable.styles";
-import IdeaTableRow from "./IdeaTableRow/IdeaTableRow";
-import { useAssignMeHotkey } from "./useAssignMeHotkey/useAssignMeHotkey";
-import UserTableCell from "./UserTableCell/UserTableCell";
-import { useToggleVoteHotkey } from "./useToggleVoteHotkey/useToggleVoteHotkey";
+import { IdeaRating } from "@/hooks/react-query/domain/group/useIdeaRatingsQueryUtils"
+import useGroupFilterStore from "@/hooks/zustand/domain/auth/group/useGroupFilterStore"
+import useIdeaSortStore from "@/hooks/zustand/domain/auth/group/useIdeaSortStore"
+import useAuthStore from "@/hooks/zustand/domain/auth/useAuthStore"
+import { TableBody, TableCell, TableContainer, TableRow } from "@mui/material"
+import { useMemo } from "react"
+import S from "./IdeaTable.styles"
+import IdeaTableRow from "./IdeaTableRow/IdeaTableRow"
+import { useAssignMeHotkey } from "./useAssignMeHotkey/useAssignMeHotkey"
+import UserTableCell from "./UserTableCell/UserTableCell"
+import { useToggleVoteHotkey } from "./useToggleVoteHotkey/useToggleVoteHotkey"
 
 interface Props {
-  ideaRatings: IdeaRating[];
+  ideaRatings: IdeaRating[]
 }
 
 const IdeaRatingsTable = (props: Props) => {
-  const authUser = useAuthStore((s) => s.authUser);
+  const authUser = useAuthStore((s) => s.authUser)
 
   const [
     hidingDone,
@@ -25,88 +25,98 @@ const IdeaRatingsTable = (props: Props) => {
     s.filter.hidingDone,
     s.filter.users,
     s.filter.onlyHighImpactVoted,
-  ]);
+  ])
 
-  const sortingBy = useIdeaSortStore((s) => s.sortingBy);
+  const sortingBy = useIdeaSortStore((s) => s.sortingBy)
 
   // PE 1/3 - put this filter + sort function in a separated file
   const visibleIdeaRatings = useMemo(() => {
-    let result = [...props.ideaRatings];
+    let result = [...props.ideaRatings]
 
     result = result.filter((r) => {
-      if (r.idea.parentId) return true; // subideas must always appear
-      if (hidingDone && r.idea.isDone) return false;
+      if (r.idea.parentId) return true // subideas must always appear
+      if (hidingDone && r.idea.isDone) return false
 
       if (filteringUsers.length > 0) {
-        const filteringIds = filteringUsers.map((u) => u.id);
-        const currentUserIds = r.idea.assignedUsers.map((u) => u.id);
+        const filteringIds = filteringUsers.map((u) => u.id)
+        const currentUserIds = r.idea.assignedUsers.map((u) => u.id)
         if (
           !filteringIds.every((filteringId) =>
             currentUserIds.includes(filteringId)
           )
         )
-          return false;
+          return false
       }
 
-      return true;
-    });
+      return true
+    })
 
     if (onlyHighImpactVoted)
       result = result.filter(
         (r) => r.idea.parentId || r.idea.highImpactVotes?.length > 0 // subideas must always appear in their table
-      );
+      )
 
     if (sortingBy.attribute === "irrelevantSince")
       result = result.sort((a, b) =>
         String(b.idea.irrelevantSince || "").localeCompare(
           String(a.idea.irrelevantSince || "")
         )
-      );
+      )
+
+    if (sortingBy.attribute === "createdAt")
+      result = result.sort((a, b) =>
+        b.idea.createdAt.localeCompare(a.idea.createdAt)
+      )
+
+    if (sortingBy.attribute === "updatedAt")
+      result = result.sort((a, b) =>
+        b.idea.updatedAt.localeCompare(a.idea.updatedAt)
+      )
 
     if (sortingBy.attribute === "avgRating") {
       result = result.sort((a, b) => {
-        const numRatingA = Number(a.avgRating);
-        const numRatingB = Number(b.avgRating);
+        const numRatingA = Number(a.avgRating)
+        const numRatingB = Number(b.avgRating)
         // if both ideas have same avg rating, it will sort by ratings count
         if (numRatingA === numRatingB) {
-          const youRatedIdeaA = a.yourRating ? 1 : 0;
+          const youRatedIdeaA = a.yourRating ? 1 : 0
           const ratingsCountA =
             youRatedIdeaA +
-            a.otherUserGroupRatings.filter((r) => r.rating).length;
+            a.otherUserGroupRatings.filter((r) => r.rating).length
 
-          const youRatedIdeaB = b.yourRating ? 1 : 0;
+          const youRatedIdeaB = b.yourRating ? 1 : 0
           const ratingsCountB =
             youRatedIdeaB +
-            b.otherUserGroupRatings.filter((r) => r.rating).length;
+            b.otherUserGroupRatings.filter((r) => r.rating).length
 
           // if both ideas have same avg rating and same ratings count, it will sort by high impact votes count
           if (ratingsCountA === ratingsCountB) {
             return a.idea.highImpactVotes?.length >
               b.idea.highImpactVotes?.length
               ? -1
-              : 1;
+              : 1
           }
 
-          return ratingsCountA > ratingsCountB ? -1 : 1;
+          return ratingsCountA > ratingsCountB ? -1 : 1
         }
 
-        return numRatingA > numRatingB ? -1 : 1;
-      });
+        return numRatingA > numRatingB ? -1 : 1
+      })
     }
 
-    return result;
+    return result
   }, [
     props.ideaRatings,
     hidingDone,
     filteringUsers,
     onlyHighImpactVoted,
     sortingBy,
-  ]);
+  ])
 
-  useAssignMeHotkey();
-  useToggleVoteHotkey();
+  useAssignMeHotkey()
+  useToggleVoteHotkey()
 
-  if (props.ideaRatings.length === 0) return <div></div>;
+  if (props.ideaRatings.length === 0) return <div></div>
 
   return (
     <TableContainer sx={{ maxHeight: 440 }}>
@@ -145,7 +155,7 @@ const IdeaRatingsTable = (props: Props) => {
         </TableBody>
       </S.Table>
     </TableContainer>
-  );
-};
+  )
+}
 
-export default IdeaRatingsTable;
+export default IdeaRatingsTable
--- src/components/GroupPage/SearchRow/IdeaSortButton/IdeaSortButton.tsx
+++ src/components/GroupPage/SearchRow/IdeaSortButton/IdeaSortButton.tsx
@@ -1,40 +1,40 @@
-import DarkButton from "@/components/_common/buttons/DarkButton/DarkButton";
-import useIdeaSortStore from "@/hooks/zustand/domain/auth/group/useIdeaSortStore";
+import DarkButton from "@/components/_common/buttons/DarkButton/DarkButton"
+import useIdeaSortStore from "@/hooks/zustand/domain/auth/group/useIdeaSortStore"
 import {
   findSortOptionByAttribute,
   ideaSortOptionsDivided,
-} from "@/types/domain/idea/ideaSortOptions";
-import { Divider, Menu, MenuItem } from "@mui/material";
-import { useRouter } from "next/router";
-import { useMemo, useState } from "react";
-import { FaSortAmountDown, FaSortAmountUpAlt } from "react-icons/fa";
+} from "@/types/domain/idea/ideaSortOptions"
+import { Divider, Menu, MenuItem } from "@mui/material"
+import { useRouter } from "next/router"
+import { useMemo, useState } from "react"
+import { FaSortAmountDown, FaSortAmountUpAlt } from "react-icons/fa"
 interface Props {
-  test?: string;
+  test?: string
 }
 
-const ariaLabel = `sort-button`;
+const ariaLabel = `sort-button`
 
 const IdeaSortButton = (props: Props) => {
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null)
 
-  const routerQuery = useRouter().query as { groupId: string };
+  const routerQuery = useRouter().query as { groupId: string }
 
   const handleOpen = (event: any) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
 
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
 
   const [sortingBy, setSortingBy] = useIdeaSortStore((s) => [
     s.sortingBy,
     s.setSortingBy,
-  ]);
+  ])
 
   const selectedSortOption = useMemo(() => {
-    return findSortOptionByAttribute(sortingBy.attribute);
-  }, [sortingBy]);
+    return findSortOptionByAttribute(sortingBy.attribute)
+  }, [sortingBy])
 
   return (
     <>
@@ -66,24 +66,25 @@ const IdeaSortButton = (props: Props) => {
       >
         {ideaSortOptionsDivided.map((sortOptions, i, a) => (
           <div key={i}>
+            {i > 0 && <Divider />}
+
             {sortOptions.map((option, j) => (
               <MenuItem
                 key={option.attribute}
                 selected={sortingBy.attribute === option.attribute}
                 onClick={() => {
-                  setSortingBy({ attribute: option.attribute, order: "desc" });
-                  handleClose();
+                  setSortingBy({ attribute: option.attribute, order: "desc" })
+                  handleClose()
                 }}
               >
                 {option.menuText}
               </MenuItem>
             ))}
-            {i % 2 === 0 && <Divider />}
           </div>
         ))}
       </Menu>
     </>
-  );
-};
+  )
+}
 
-export default IdeaSortButton;
+export default IdeaSortButton
