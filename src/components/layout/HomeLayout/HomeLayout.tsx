bcb5a2f0cd381dc4606e2af509d5cf08807baa1a
Paulo Endoh <paulo.endoh@gmail.com>
2022-09-21 17:25:53 -0300

    fix - trying to access authUser.id before it is loaded

--- src/components/layout/HomeLayout/HomeLayout.tsx
+++ src/components/layout/HomeLayout/HomeLayout.tsx
@@ -1,15 +1,20 @@
-import Flex from "@/components/_common/flexboxes/Flex";
-import { Box } from "@mui/material";
-import React from "react";
-import GlobalDialogs from "../dialogs/GlobalDialogs/GlobalDialogs";
-import Navbar from "../Navbar/Navbar";
-import Sidebar from "../Sidebar/Sidebar";
+import Flex from "@/components/_common/flexboxes/Flex"
+import useAuthStore from "@/hooks/zustand/domain/auth/useAuthStore"
+import { Box } from "@mui/material"
+import React from "react"
+import GlobalDialogs from "../dialogs/GlobalDialogs/GlobalDialogs"
+import Navbar from "../Navbar/Navbar"
+import Sidebar from "../Sidebar/Sidebar"
 
 interface Props {
-  children?: React.ReactNode;
+  children?: React.ReactNode
 }
 
 const HomeLayout = (props: Props) => {
+  const authUser = useAuthStore((s) => s.authUser)
+
+  if (!authUser) return null
+
   return (
     <div>
       <Navbar />
@@ -22,7 +27,7 @@ const HomeLayout = (props: Props) => {
         <GlobalDialogs />
       </Flex>
     </div>
-  );
-};
+  )
+}
 
-export default HomeLayout;
+export default HomeLayout
