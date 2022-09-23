8b6c78c551122477907de5b57fa9bacc4e47b318
Paulo Endoh <paulo.endoh@gmail.com>
2022-09-23 18:11:59 -0300

    stop refetching ideas on focus -- since we have websockets now :D

--- src/hooks/react-query/domain/group/idea/useGroupIdeasQuery.ts
+++ src/hooks/react-query/domain/group/idea/useGroupIdeasQuery.ts
@@ -1,18 +1,24 @@
-import IdeaDto from "@/types/domain/group/tab/idea/IdeaDto";
-import myAxios from "@/utils/axios/myAxios";
-import queryKeys from "@/utils/queryKeys";
-import urls from "@/utils/urls";
-import { useQuery } from "react-query";
+import IdeaDto from "@/types/domain/group/tab/idea/IdeaDto"
+import myAxios from "@/utils/axios/myAxios"
+import queryKeys from "@/utils/queryKeys"
+import urls from "@/utils/urls"
+import { useQuery } from "react-query"
 
 const useGroupIdeasQuery = (groupId: string) => {
-  const query = useQuery(queryKeys.groupIdeas(groupId), async () => {
-    if (!groupId) return [];
+  const query = useQuery(
+    queryKeys.groupIdeas(groupId),
+    async () => {
+      if (!groupId) return []
 
-    return myAxios
-      .get<IdeaDto[]>(urls.api.groupIdeas(groupId))
-      .then((res) => res.data);
-  });
-  return query;
-};
+      return myAxios
+        .get<IdeaDto[]>(urls.api.groupIdeas(groupId))
+        .then((res) => res.data)
+    },
+    {
+      refetchOnWindowFocus: false,
+    }
+  )
+  return query
+}
 
-export default useGroupIdeasQuery;
+export default useGroupIdeasQuery
