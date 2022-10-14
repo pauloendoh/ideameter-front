4329cf039621179f5aa8d9aba45435116c372367
Paulo Endoh <paulo.endoh@gmail.com>
2022-10-14 00:23:51 -0300

    feat/cl8ljvgsi07990ms7ujdvj16m - remove show on hover

--- src/components/GroupPage/GroupTabContent/IdeaRatingsTable/IdeaTableRow/IdeaTableRow.tsx
+++ src/components/GroupPage/GroupTabContent/IdeaRatingsTable/IdeaTableRow/IdeaTableRow.tsx
@@ -1,4 +1,3 @@
-import FlexVCenter from "@/components/_common/flexboxes/FlexVCenter"
 import useSaveIdeaMutation from "@/hooks/react-query/domain/group/tab/idea/useSaveIdeaMutation"
 import { IdeaRating } from "@/hooks/react-query/domain/group/useIdeaRatingsQueryUtils"
 import useIdeaDialogStore from "@/hooks/zustand/dialogs/useIdeaDialogStore"
@@ -98,17 +97,11 @@ const IdeaTableRow = (props: Props) => {
       </TableCell>
       <AvgRatingTableCell ideaRating={props.ideaRating} />
       <TableCell align="center">
-        {isHoveringIdeaId ? (
-          <RatingInput
-            idea={props.ideaRating.idea}
-            groupId={query.groupId}
-            parentId={props.ideaRating.idea.parentId}
-          />
-        ) : (
-          <FlexVCenter sx={{ width: 64, justifyContent: "center" }}>
-            {props.ideaRating.yourRating}
-          </FlexVCenter>
-        )}
+        <RatingInput
+          idea={props.ideaRating.idea}
+          groupId={query.groupId}
+          parentId={props.ideaRating.idea.parentId}
+        />
       </TableCell>
       {props.ideaRating.otherUserGroupRatings.map((userGroupRating, index) => (
         <TableCell key={JSON.stringify(userGroupRating)} align="center">
