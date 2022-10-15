d5c6b09df5541e4bd6158854cc03f219c7887ab9
Dickson <dicksonmelo@gmail.com>
2022-10-15 17:39:00 -0300

    chore: create assigned to me page

--- src/pages/assigned-to-me/[userId]/index.tsx
+++ src/pages/assigned-to-me/[userId]/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react"
 
 const AssignedToMePage = () => {
-  return (
-    <div>AssignedToMePage</div>
-  )
+  return <div>AssignedToMePage</div>
 }
 
 export default AssignedToMePage
