00bf4b02e5eb7de61b343526f2ddd3d5883cca30
Paulo Endoh <paulo.endoh@gmail.com>
2022-10-20 00:34:04 -0300

    feat/cl83mm9pp08320mu20rvidvl8 - max height rich text editor

--- src/components/layout/dialogs/IdeaDialog/IdeaDialogLeftCol/styles.ts
+++ src/components/layout/dialogs/IdeaDialog/IdeaDialogLeftCol/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@mui/material";
+import { Box, styled } from "@mui/material"
 
 export const styles = {
   MantineRteContainer: styled(Box)(({ theme }) => ({
@@ -36,5 +36,9 @@ export const styles = {
       borderRadius: "0px 0px 4px 4px",
       color: theme.palette.grey[100],
     },
+    ".ql-editor": {
+      maxHeight: "300px",
+      overflowY: "auto",
+    },
   })),
-};
+}
