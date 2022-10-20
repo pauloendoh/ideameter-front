9a7048361abbbd5b09934e77c3f38b968131c68f
Paulo Endoh <paulo.endoh@gmail.com>
2022-10-20 00:42:56 -0300

    feat/cl83mm9pp08320mu20rvidvl8 - removing 'on fire since'

--- src/components/GroupPage/GroupTabContent/IdeaRatingsTable/IdeaTableRow/AvgRatingTableCell/AvgRatingTableCell.tsx
+++ src/components/GroupPage/GroupTabContent/IdeaRatingsTable/IdeaTableRow/AvgRatingTableCell/AvgRatingTableCell.tsx
@@ -1,35 +1,21 @@
-import { IdeaRating } from "@/hooks/react-query/domain/group/useIdeaRatingsQueryUtils";
-import { getOnFireSinceFormat } from "@/utils/domain/idea/getOnFireSinceFormat";
-import { TableCell, Tooltip } from "@mui/material";
-import { DateTime } from "luxon";
-import { useMemo } from "react";
+import { IdeaRating } from "@/hooks/react-query/domain/group/useIdeaRatingsQueryUtils"
+import { TableCell } from "@mui/material"
 
 interface Props {
-  ideaRating: IdeaRating;
+  ideaRating: IdeaRating
 }
 
 const AvgRatingTableCell = (props: Props) => {
-  const { onFireSince } = props.ideaRating.idea;
-
-  const tooltipTitle = useMemo(() => {
-    if (!onFireSince) return "";
-
-    const format = getOnFireSinceFormat(onFireSince);
-    return DateTime.fromISO(onFireSince).toFormat(format);
-  }, [props.ideaRating.idea.onFireSince]);
   return (
-    <Tooltip title={tooltipTitle} arrow placement="top">
-      <TableCell
-        align="center"
-        sx={{
-          background:
-            Number(props.ideaRating.avgRating) >= 2.5 ? "#232323" : undefined,
-        }}
-      >
-        {props.ideaRating.avgRating}
-      </TableCell>
-    </Tooltip>
-  );
-};
+    <TableCell
+      align="center"
+      sx={{
+        background: Number(props.ideaRating.avgRating) >= 2.5 ? "#232323" : undefined,
+      }}
+    >
+      {props.ideaRating.avgRating}
+    </TableCell>
+  )
+}
 
-export default AvgRatingTableCell;
+export default AvgRatingTableCell
