be5dc84056f932701f0d8c823154e8ee29cf0e0f
Paulo Endoh <paulo.endoh@gmail.com>
2022-11-03 01:04:32 -0300

    fix/cl9foks8x44930mp67wg1bgki - after deleting tab -> go to next available tab

--- src/hooks/react-query/domain/group/tab/useDeleteTabMutation.ts
+++ src/hooks/react-query/domain/group/tab/useDeleteTabMutation.ts
@@ -1,14 +1,19 @@
-import useSnackbarStore from "@/hooks/zustand/useSnackbarStore";
-import TabDto from "@/types/domain/group/tab/TabDto";
-import { pushOrRemove } from "@/utils/array/pushOrRemove";
-import myAxios from "@/utils/axios/myAxios";
-import queryKeys from "@/utils/queryKeys";
-import urls from "@/utils/urls";
-import { useMutation, useQueryClient } from "react-query";
+import { useRouterQueryString } from "@/hooks/utils/useRouterQueryString"
+import useSnackbarStore from "@/hooks/zustand/useSnackbarStore"
+import TabDto from "@/types/domain/group/tab/TabDto"
+import { pushOrRemove } from "@/utils/array/pushOrRemove"
+import myAxios from "@/utils/axios/myAxios"
+import queryKeys from "@/utils/queryKeys"
+import urls from "@/utils/urls"
+import { useRouter } from "next/router"
+import { useMutation, useQueryClient } from "react-query"
 
 const useDeleteTabMutation = () => {
-  const queryClient = useQueryClient();
-  const { setSuccessMessage, setErrorMessage } = useSnackbarStore();
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  const { setSuccessMessage, setErrorMessage } = useSnackbarStore()
+
+  const { groupId, tabId } = useRouterQueryString()
 
   return useMutation(
     (payload: TabDto) =>
@@ -18,27 +23,37 @@ const useDeleteTabMutation = () => {
         })
         .then((res) => res.data),
     {
-      onSuccess: (savedTab) => {
-        setSuccessMessage("Tab deleted!");
+      onSuccess: (deletedTab) => {
+        setSuccessMessage("Tab deleted!")
 
         const groupTabs = queryClient.getQueryData<TabDto[]>(
-          queryKeys.groupTabs(savedTab.groupId)
-        );
+          queryKeys.groupTabs(deletedTab.groupId)
+        )
 
-        if (!groupTabs) return;
+        if (!groupTabs) return
 
-        const newGroupTabs = pushOrRemove(groupTabs, savedTab, "id");
+        const newGroupTabs = pushOrRemove(groupTabs, deletedTab, "id")
 
-        queryClient.setQueryData(
-          queryKeys.groupTabs(savedTab.groupId),
-          newGroupTabs
-        );
+        queryClient.setQueryData(queryKeys.groupTabs(deletedTab.groupId), newGroupTabs)
+
+        if (!groupId || tabId !== deletedTab.id) {
+          return
+        }
+
+        const nextAvailableTab = groupTabs.find((t) => t.id !== tabId)
+        if (nextAvailableTab) {
+          router.push(urls.pages.groupTab(groupId, nextAvailableTab.id))
+          return
+        }
+
+        queryClient.invalidateQueries(queryKeys.groupIdeas(groupId))
+        queryClient.invalidateQueries(queryKeys.groupTabs(groupId))
       },
       onError: (err) => {
-        setErrorMessage(JSON.stringify(err));
+        setErrorMessage(JSON.stringify(err))
       },
     }
-  );
-};
+  )
+}
 
-export default useDeleteTabMutation;
+export default useDeleteTabMutation
--- src/hooks/utils/useRouterQueryString.ts
+++ src/hooks/utils/useRouterQueryString.ts
@@ -1,6 +1,11 @@
 import { useRouter } from "next/router"
 
+type UrlQueryParams = { groupId?: string; tabId?: string; ideaId?: string }
+
 export const useRouterQueryString = () => {
   const router = useRouter()
-  return router.query as { groupId?: string; tabId?: string; ideaId?: string }
+  if (router?.isReady) return router?.query as UrlQueryParams
+
+  // necessary to pass through tests
+  return {} as UrlQueryParams
 }
