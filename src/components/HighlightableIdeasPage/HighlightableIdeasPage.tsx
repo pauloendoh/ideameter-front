678c256193ab38156fdd0146830df1c4c84c34a1
Paulo Endoh <paulo.endoh@gmail.com>
2023-04-26 02:48:23 -0300

    Highlightable ideas [clgx6al0d000e0i0kl6gl87qd]

--- src/components/HighlightableIdeasPage/HighlightableIdeasPage.tsx
+++ src/components/HighlightableIdeasPage/HighlightableIdeasPage.tsx
@@ -1,15 +1,20 @@
 import useHighlightableIdeasQuery from "@/hooks/react-query/domain/idea-highlight/useHighlightableIdeasQuery"
+import useIdeaHighlightsQuery from "@/hooks/react-query/domain/idea-highlight/useIdeaHighlightsQuery"
 import {
   Container,
+  FormControlLabel,
   Paper,
+  Switch,
   Table,
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
   TableRow,
   Typography,
 } from "@mui/material"
+import { useMemo, useState } from "react"
 import FlexVCenter from "../_common/flexboxes/FlexVCenter"
 import HomeLayout from "../layout/HomeLayout/HomeLayout"
 import HighlightableIdeaRow from "./HighlightableIdeaRow/HighlightableIdeaRow"
@@ -18,6 +23,18 @@ type Props = {}
 
 const HighlightableIdeasPage = (props: Props) => {
   const { data: ideas } = useHighlightableIdeasQuery()
+  const [showHighlighted, setShowHighlighted] = useState(false)
+
+  const { data: ideaHighlights } = useIdeaHighlightsQuery()
+
+  const filteredIdeas = useMemo(() => {
+    if (!showHighlighted) return ideas
+
+    return ideas?.filter((idea) =>
+      ideaHighlights?.some((ideaHighlight) => ideaHighlight.ideaId === idea.id)
+    )
+  }, [ideas, showHighlighted, ideaHighlights])
+
   return (
     <HomeLayout>
       <Container>
@@ -60,7 +77,7 @@ const HighlightableIdeasPage = (props: Props) => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {ideas?.map((idea, index) => (
+                  {filteredIdeas?.map((idea, index) => (
                     <HighlightableIdeaRow
                       key={idea.id}
                       idea={idea}
@@ -71,25 +88,25 @@ const HighlightableIdeasPage = (props: Props) => {
               </Table>
             </TableContainer>
 
-            {/* <TableFooter
-          sx={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "flex-end",
-            p: 1,
-          }}
-        >
-          <FormControlLabel
-            control={
-              <Switch
-                defaultChecked={showCompleted}
-                checked={showCompleted}
-                onClick={() => setShowCompleted(!showCompleted)}
+            <TableFooter
+              sx={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-end",
+                p: 1,
+              }}
+            >
+              <FormControlLabel
+                control={
+                  <Switch
+                    defaultChecked={showHighlighted}
+                    checked={showHighlighted}
+                    onClick={() => setShowHighlighted(!showHighlighted)}
+                  />
+                }
+                label={`Highlighted only`}
               />
-            }
-            label={`Completed ideas`}
-          />
-        </TableFooter> */}
+            </TableFooter>
           </FlexVCenter>
         </Paper>
       </Container>
