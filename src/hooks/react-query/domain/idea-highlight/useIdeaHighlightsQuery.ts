e9ef25733f673fc785ed79dccaf76ce4300053c1
Paulo Endoh <paulo.endoh@gmail.com>
2023-04-26 02:34:30 -0300

    Highlightable ideas [clgx6al0d000e0i0kl6gl87qd]

--- src/hooks/react-query/domain/idea-highlight/useIdeaHighlightsQuery.ts
+++ src/hooks/react-query/domain/idea-highlight/useIdeaHighlightsQuery.ts
@@ -2,6 +2,7 @@ import myAxios from "@/utils/axios/myAxios"
 import queryKeys from "@/utils/queryKeys"
 import urls from "@/utils/urls"
 import { useQuery } from "react-query"
+import { IdeaHighlightDto } from "./types/IdeaHighlightDto"
 
 const useIdeaHighlightsQuery = () => {
   const query = useQuery(queryKeys.ideaHighlights, async () =>
