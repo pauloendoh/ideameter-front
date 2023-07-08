0df09de72444420e89e4e7719044c86cfd5a1502
Paulo Endoh <paulo.endoh@gmail.com>
2023-07-08 00:16:48 -0300

    fix multiselect ideas

--- src/components/GroupPage/GroupTabContent/IdeaRatingsTable/useMultiSelectIdeas/useMultiSelectIdeas.ts
+++ src/components/GroupPage/GroupTabContent/IdeaRatingsTable/useMultiSelectIdeas/useMultiSelectIdeas.ts
@@ -1,59 +1,12 @@
 import useSelectedIdeasStore from "@/hooks/zustand/domain/idea/useSelectedIdeasStore"
 
 const useMultiSelectIdeas = () => {
-  const { selectedIdeaIds, setSelectedIdeaIds } = useSelectedIdeasStore()
-
-  const onCtrlClick = (ideaId: string) => {
-    const ids = [...selectedIdeaIds]
-
-    // remove
-    if (selectedIdeaIds.includes(ideaId))
-      setSelectedIdeaIds(ids.filter((id) => id !== ideaId))
-    // add
-    else setSelectedIdeaIds([...ids, ideaId])
-  }
+  const { selectedIdeaIds, setSelectedIdeaIds, onCtrlClick, onShiftClick } =
+    useSelectedIdeasStore()
 
   const idIsSelected = (ideaId: string) => selectedIdeaIds.includes(ideaId)
 
   const clearSelectedIds = () => setSelectedIdeaIds([])
-
-  const onShiftClick = (ideaIds: string[], selectedId: string) => {
-    if (selectedIdeaIds.length === 0) {
-      setSelectedIdeaIds([selectedId])
-      return
-    }
-
-    if (selectedIdeaIds.length === 1 && selectedIdeaIds[0] === selectedId) {
-      setSelectedIdeaIds([])
-      return
-    }
-
-    if (selectedIdeaIds.includes(selectedId)) {
-      setSelectedIdeaIds([selectedId])
-      return
-    }
-
-    const firstSelectedIdx = ideaIds.findIndex((id) =>
-      selectedIdeaIds.includes(id)
-    )
-    const lastSelectedId = [...ideaIds]
-      .reverse()
-      .find((id) => selectedIdeaIds.includes(id))
-    const lastSelectedIdx = [...ideaIds].findIndex(
-      (id) => id === lastSelectedId
-    )
-
-    const clickedIdx = [...ideaIds].findIndex((id) => id === selectedId)
-
-    const finalIds = ideaIds.filter((_, idx) => {
-      if (firstSelectedIdx < clickedIdx)
-        return idx >= firstSelectedIdx && idx <= clickedIdx
-
-      return idx >= clickedIdx && idx <= lastSelectedIdx
-    })
-
-    setSelectedIdeaIds(finalIds)
-  }
 
   return { onCtrlClick, onShiftClick, idIsSelected, clearSelectedIds }
 }
--- src/hooks/zustand/domain/idea/useSelectedIdeasStore.ts
+++ src/hooks/zustand/domain/idea/useSelectedIdeasStore.ts
@@ -3,11 +3,65 @@ import create from "zustand"
 interface ISelectedIdeasStore {
   selectedIdeaIds: string[]
   setSelectedIdeaIds: (ids: string[]) => void
+  onCtrlClick: (ideaId: string) => void
+  onShiftClick: (ideaIds: string[], selectedId: string) => void
 }
 
-const useSelectedIdeasStore = create<ISelectedIdeasStore>((set) => ({
+const useSelectedIdeasStore = create<ISelectedIdeasStore>((set, get) => ({
   selectedIdeaIds: [],
   setSelectedIdeaIds: (selectedIdeaIds) => set({ selectedIdeaIds }),
+
+  onCtrlClick: (ideaId: string) => {
+    const { selectedIdeaIds, setSelectedIdeaIds } = get()
+
+    const ids = [...selectedIdeaIds]
+
+    // remove
+    if (selectedIdeaIds.includes(ideaId))
+      setSelectedIdeaIds(ids.filter((id) => id !== ideaId))
+    // add
+    else setSelectedIdeaIds([...ids, ideaId])
+  },
+
+  onShiftClick: (ideaIds: string[], selectedId: string) => {
+    const { selectedIdeaIds, setSelectedIdeaIds } = get()
+
+    if (selectedIdeaIds.length === 0) {
+      setSelectedIdeaIds([selectedId])
+      return
+    }
+
+    if (selectedIdeaIds.length === 1 && selectedIdeaIds[0] === selectedId) {
+      setSelectedIdeaIds([])
+      return
+    }
+
+    if (selectedIdeaIds.includes(selectedId)) {
+      setSelectedIdeaIds([selectedId])
+      return
+    }
+
+    const firstSelectedIdx = ideaIds.findIndex((id) =>
+      selectedIdeaIds.includes(id)
+    )
+    const lastSelectedId = [...ideaIds]
+      .reverse()
+      .find((id) => selectedIdeaIds.includes(id))
+    const lastSelectedIdx = [...ideaIds].findIndex(
+      (id) => id === lastSelectedId
+    )
+
+    const clickedIdx = [...ideaIds].findIndex((id) => id === selectedId)
+
+    const finalIds = ideaIds.filter((_, idx) => {
+      if (firstSelectedIdx < clickedIdx)
+        return idx >= firstSelectedIdx && idx <= clickedIdx
+
+      return idx >= clickedIdx && idx <= lastSelectedIdx
+    })
+
+    setSelectedIdeaIds(finalIds)
+  },
 }))
 
 const initialState = useSelectedIdeasStore.getState()
