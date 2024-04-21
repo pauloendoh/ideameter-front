d657dcdcae6bd1de08b1af1a1994cb11dadd086b
Paulo Endoh <paulo.endoh@gmail.com>
2024-04-21 16:17:50 -0300

    my ratings -- mostrar 'groupName - tabName' [clv9vzr1t0000y50l6yl3cqro]

--- src/components/MyRatingsPage/MyRatingItem/MyRatingItem.tsx
+++ src/components/MyRatingsPage/MyRatingItem/MyRatingItem.tsx
@@ -11,12 +11,16 @@ type Props = {
 }
 
 const MyRatingItem = ({ assign, index, ...props }: Props) => {
+  const idea = assign.idea
+  const group = assign.group
+  const tab = assign.tab
+
   const { hovered, ref: hoverRef } = useHover()
 
   return (
     <FlexCol
       ref={hoverRef}
-      key={assign.idea.id}
+      key={idea.id}
       sx={{
         borderTop: index === 0 ? "none" : "1px solid",
         py: 1,
@@ -24,11 +28,28 @@ const MyRatingItem = ({ assign, index, ...props }: Props) => {
       }}
     >
       <Flex justify={"space-between"}>
-        <Typography>{assign.idea.name}</Typography>
+        <span>
+          <span
+            style={{
+              background: "gray",
+              padding: "2px 4px",
+              borderRadius: 4,
+            }}
+          >
+            {group.name} - {tab.name}
+          </span>
+          <span
+            style={{
+              marginLeft: 8,
+            }}
+          >
+            {idea.name}
+          </span>
+        </span>
         <MyRatingItemMenu isHovering={hovered} ratingId={assign.myRating.id} />
       </Flex>
-      <Typography>Is done: {String(assign.idea.isDone)}</Typography>
-      <Typography>Is archived: {String(assign.idea.isArchived)}</Typography>
+      <Typography>Is done: {String(idea.isDone)}</Typography>
+      <Typography>Is archived: {String(idea.isArchived)}</Typography>
     </FlexCol>
   )
 }
