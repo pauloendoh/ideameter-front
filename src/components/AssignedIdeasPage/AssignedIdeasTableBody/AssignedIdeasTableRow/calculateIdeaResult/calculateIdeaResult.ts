dae35c1ef8fe7b9e102869ab2c0f7e72c9f5b894
Paulo Endoh <paulo.endoh@gmail.com>
2024-07-30 00:11:33 -0300

    improve calculation

--- src/components/AssignedIdeasPage/AssignedIdeasTableBody/AssignedIdeasTableRow/calculateIdeaResult/calculateIdeaResult.ts
+++ src/components/AssignedIdeasPage/AssignedIdeasTableBody/AssignedIdeasTableRow/calculateIdeaResult/calculateIdeaResult.ts
@@ -4,7 +4,7 @@ export const calculateIdeaResult = (idea: IdeaDto): number => {
   const rewarding = idea.rewarding || 0
   const discomfortZone = idea.discomfortZone || 0
 
-  const result = Math.pow(rewarding, 2) * discomfortZone
+  const result = Math.pow(rewarding, 2) * Math.pow(discomfortZone, 2)
 
   return Math.round(result)
 }
