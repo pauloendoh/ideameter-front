aa9345a76f1242ee9982079ac30e86d02a4e6b54
Paulo Endoh <paulo.endoh@gmail.com>
2024-09-15 13:43:05 -0300

    idea dialog -- quando ideia completada e sem assigned user, mostrar em vermelho e um info icon [cm0zek4kg000uvm0lpwctazg9]

--- src/components/layout/dialogs/IdeaDialog/IdeaDialogAssignedUsers/IdeaDialogAssignedUsers.tsx
+++ src/components/layout/dialogs/IdeaDialog/IdeaDialogAssignedUsers/IdeaDialogAssignedUsers.tsx
@@ -5,12 +5,14 @@ import FlexVCenter from "@/components/_common/flexboxes/FlexVCenter"
 import { useRouterQueryString } from "@/hooks/utils/useRouterQueryString"
 import useIdeaAssignmentStore from "@/hooks/zustand/dialogs/useIdeaAssignmentStore"
 import IdeaDto from "@/types/domain/group/tab/idea/IdeaDto"
-import { Avatar, Typography } from "@mui/material"
+import { Avatar, Box, Tooltip, Typography } from "@mui/material"
+import { useMemo } from "react"
 import { UseFormSetValue, UseFormWatch } from "react-hook-form"
 import { FaUserAlt } from "react-icons/fa"
-import { MdOutlineAdd } from "react-icons/md"
+import { MdOutlineAdd, MdWarning } from "react-icons/md"
 
 interface Props {
+  // PE 1/3 - rename to "values" or "idea"
   watch: UseFormWatch<IdeaDto>
   setValue: UseFormSetValue<IdeaDto>
 }
@@ -18,11 +20,37 @@ interface Props {
 const IdeaDialogAssignedUsers = ({ watch, setValue }: Props) => {
   const openAssignDialog = useIdeaAssignmentStore((s) => s.openDialog)
   const { groupId } = useRouterQueryString()
+
+  const showAlert = useMemo(() => {
+    return watch("assignedUsers").length === 0 && watch("isDone")
+  }, [watch("assignedUsers"), watch("isDone")])
+
   return (
     <FlexCol gap={1}>
-      <FlexVCenter gap={1}>
+      <FlexVCenter
+        gap={1}
+        sx={(theme) => ({
+          color: showAlert ? theme.palette.warning.main : undefined,
+        })}
+      >
         <FaUserAlt />
         <Typography>Assigned</Typography>
+        {showAlert && (
+          <Tooltip
+            title="Please assign at least one user before completing"
+            arrow
+          >
+            <Box
+              sx={{
+                position: "relative",
+                top: 2,
+                left: -2,
+              }}
+            >
+              <MdWarning />
+            </Box>
+          </Tooltip>
+        )}
       </FlexVCenter>
       <Flex gap={1}>
         {watch("assignedUsers").map((user) => (
