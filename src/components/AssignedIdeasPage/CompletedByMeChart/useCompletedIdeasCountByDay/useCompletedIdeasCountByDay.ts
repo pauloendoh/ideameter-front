449117dd89931a4616ab1688751cbb23ccf2dbcd
Paulo Endoh <paulo.endoh@gmail.com>
2024-09-15 15:46:22 -0300

    completed ideas chart -- daily -- grafico fora de ordem [cm13x87w90001xe0lgrsizm5q]

--- src/components/AssignedIdeasPage/CompletedByMeChart/useCompletedIdeasCountByDay/useCompletedIdeasCountByDay.ts
+++ src/components/AssignedIdeasPage/CompletedByMeChart/useCompletedIdeasCountByDay/useCompletedIdeasCountByDay.ts
@@ -17,60 +17,6 @@ export const useCompletedIdeasCountByDay = (
   )
 
   const result = useMemo(() => {
-    // const startingDays: Date[] = []
-
-    // const lastMonday = new Date()
-    // lastMonday.setDate(lastMonday.getDate() - lastMonday.getDay() + 1) // getDay (of week)
-
-    // // past 9 weeks + current week
-    // for (let i = 0; i < 10; i++) {
-    //   const day = new Date()
-    //   day.setDate(lastMonday.getDate() - i * 7)
-    //   startingDays.push(day)
-    // }
-
-    // const sortedStartingDays = startingDays.sort(
-    //   (a, b) => a.getTime() - b.getTime()
-    // )
-
-    // const result: IdeaCount[] = []
-
-    // for (const day of sortedStartingDays) {
-    //   const startingDay = new Date(day)
-    //   const finishedDay = new Date(day)
-    //   finishedDay.setDate(finishedDay.getDate() + 6)
-
-    //   const formattedStartingDay = startingDay.toLocaleDateString("en-US", {
-    //     month: "short",
-    //     day: "numeric",
-    //   })
-
-    //   const formattedFinishedDay = finishedDay.getDate()
-
-    //   const ideaCount: IdeaCount = {
-    //     // key: day format "MMM/dd~dd"
-    //     key: `${formattedStartingDay}-${formattedFinishedDay}`,
-    //     count: 0,
-    //   }
-
-    //   const ideasFromWeek = completedIdeas.filter((idea) => {
-    //     const completedAt = new Date(idea.idea.completedAt!)
-    //     return (
-    //       completedAt.getTime() >= startingDay.getTime() &&
-    //       completedAt.getTime() <= finishedDay.getTime()
-    //     )
-    //   })
-
-    //   ideaCount.count = ideasFromWeek.length
-    //   if (type === "complexity") {
-    //     ideaCount.count = ideasFromWeek.reduce((acc, curr) => {
-    //       return acc + curr.idea.complexity
-    //     }, 0)
-    //   }
-
-    //   result.push(ideaCount)
-    // }
-
     const result: IdeaCount[] = []
 
     // last 6 days + today
@@ -106,8 +52,12 @@ export const useCompletedIdeasCountByDay = (
       result.push(ideaCount)
     }
 
-    return result.sort((a, b) => a.key.localeCompare(b.key))
+    result.reverse()
+
+    return result
   }, [completedIdeas, type])
+
+  console.log(result)
 
   return result
 }
