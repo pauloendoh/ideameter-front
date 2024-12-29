f3db8804ba3656efb11aab975b778d5aefdcf554
Paulo Endoh <paulo.endoh@gmail.com>
2024-12-29 00:31:02 -0300

    insights -- aba principal é o 'missing ratings' [cm3arhlw7001grp0lcxck6qe3]

--- src/components/layout/dialogs/GroupInsightsDialog/GroupInsightsDialogContent/GroupInsightsDialogContent.tsx
+++ src/components/layout/dialogs/GroupInsightsDialog/GroupInsightsDialogContent/GroupInsightsDialogContent.tsx
@@ -27,19 +27,22 @@ const GroupInsightsDialogContent = (props: Props) => {
           onChange={handleChange}
           aria-label="basic tabs example"
         >
-          <Tab label="Interest similarity" {...a11yProps(0)} />
-          <Tab label="Missing Ratings" {...a11yProps(1)} />
-          <Tab label="Last comments" {...a11yProps(2)} />
+          <Tab label="Missing Ratings" {...a11yProps(0)} />
+          <Tab label="Last comments" {...a11yProps(1)} />
+          <Tab label="Interest similarity" {...a11yProps(2)} />
         </Tabs>
       </Box>
+
       <TabPanel value={value} index={0}>
-        <InterestSimilarityTab groupId={props.group.id!} />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
         <MissingRatingsTab groupId={props.group.id!} />
       </TabPanel>
-      <TabPanel value={value} index={2}>
+
+      <TabPanel value={value} index={1}>
         <LastCommentsTab groupId={props.group.id!} />
+      </TabPanel>
+
+      <TabPanel value={value} index={2}>
+        <InterestSimilarityTab groupId={props.group.id!} />
       </TabPanel>
     </Box>
   )
