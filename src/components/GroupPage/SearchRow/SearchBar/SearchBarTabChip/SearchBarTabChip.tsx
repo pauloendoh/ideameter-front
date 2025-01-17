faf29aad83ef3406dc14f9ab6bde6eb0cce2e272
Paulo Endoh <paulo.endoh@gmail.com>
2025-01-16 22:04:40 -0300

    search -- quando clicar no tab name, ir para aquela tab [cm3aextt9000cql0lfmqsm6gr]

--- src/components/GroupPage/SearchRow/SearchBar/SearchBarTabChip/SearchBarTabChip.tsx
+++ src/components/GroupPage/SearchRow/SearchBar/SearchBarTabChip/SearchBarTabChip.tsx
@@ -1,4 +1,6 @@
+import MyNextLink from "@/components/_common/MyNextLink/MyNextLink"
 import TabDto from "@/types/domain/group/tab/TabDto"
+import urls from "@/utils/urls"
 import { Typography, useTheme } from "@mui/material"
 
 interface Props {
@@ -11,20 +13,32 @@ const SearchBarTabChip = (props: Props) => {
   if (!props.tab) return null
 
   return (
-    <Typography
-      sx={{
-        px: 1,
-        py: 0.25,
-        borderRadius: 0.5,
-        background: theme.palette.grey[800],
-        justifyContent: "center",
-        width: "100px",
-        height: "fit-content",
-      }}
-      noWrap
-    >
-      {props.tab.name}
-    </Typography>
+    <MyNextLink href={urls.pages.groupTab(props.tab.groupId, props.tab.id)}>
+      <a
+        style={{
+          textDecoration: "none",
+          color: "inherit",
+        }}
+        onClick={(e) => {
+          e.stopPropagation()
+        }}
+      >
+        <Typography
+          sx={{
+            px: 1,
+            py: 0.25,
+            borderRadius: 0.5,
+            background: theme.palette.grey[800],
+            justifyContent: "center",
+            width: "100px",
+            height: "fit-content",
+          }}
+          noWrap
+        >
+          {props.tab.name}
+        </Typography>
+      </a>
+    </MyNextLink>
   )
 }
 
