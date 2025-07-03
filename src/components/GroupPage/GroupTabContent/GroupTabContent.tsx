07d205c7bdbe114ea4999aba31c63bcbdfc07450
Paulo Endoh <paulo.endoh@gmail.com>
2025-07-02 22:50:53 -0300

    (testar) ao clicar para adicionar uma ideia -> vai abrir apenas um menu ao invés de um dialog, pedindo para inserir o nome da ideia (assim como no Trello e Azure DevOps); depois ele abre o dialog?.. [cl97i6jm1152230mwqitixmgjo]

--- src/components/GroupPage/GroupTabContent/GroupTabContent.tsx
+++ src/components/GroupPage/GroupTabContent/GroupTabContent.tsx
@@ -1,16 +1,12 @@
 import FlexCol from "@/components/_common/flexboxes/FlexCol"
 import FlexVCenter from "@/components/_common/flexboxes/FlexVCenter"
-import { useNewIdeaHotkey } from "@/hooks/hotkeys/useNewIdeaHotkey/useNewIdeaHotkey"
 import useTabIdeasQuery from "@/hooks/react-query/domain/group/tab/idea/useTabIdeasQuery"
-import useIdeaDialogStore from "@/hooks/zustand/dialogs/useIdeaDialogStore"
-import useAuthStore from "@/hooks/zustand/domain/auth/useAuthStore"
 import useGroupFilterStore from "@/hooks/zustand/domain/group/useGroupFilterStore"
-import useAutoScrollStore from "@/hooks/zustand/useAutoScrollStore"
-import { buildIdeaDto } from "@/types/domain/group/tab/idea/IdeaDto"
-import { Box, Button, FormControlLabel, FormGroup, Switch } from "@mui/material"
+import { Box, FormControlLabel, FormGroup, Switch } from "@mui/material"
 import { useRouter } from "next/router"
 import useIdeaTableItemsQueryUtils from "../../../hooks/react-query/domain/group/useIdeaTableItemsQueryUtils"
 import IdeaTable from "./IdeaTable/IdeaTable"
+import { NewIdeaMenuButton } from "./NewIdeaMenuButton/NewIdeaMenuButton"
 
 interface Props {
   tabId: string
@@ -26,8 +22,6 @@ const GroupTabContent = (props: Props) => {
 
   const ideaRatings = useIdeaTableItemsQueryUtils(query.groupId, props.tabId)
 
-  const { openDialog } = useIdeaDialogStore()
-
   const [filter, toggleOnlyCompletedIdeas, toggleGhostRatings] =
     useGroupFilterStore((s) => [
       s.filter,
@@ -35,27 +29,12 @@ const GroupTabContent = (props: Props) => {
       s.toggleGhostRatings,
     ])
 
-  useNewIdeaHotkey(props.tabId)
-
-  const [autoScrollIsDisabled, toggleAutoScroll] = useAutoScrollStore((s) => [
-    s.isDisabled,
-    s.toggleIsDisabled,
-  ])
-
-  const { authUser } = useAuthStore()
-
   return (
     <FlexCol gap={2}>
       <Box>{ideas && <IdeaTable ideaRatings={ideaRatings} />}</Box>
 
       <FlexVCenter ml={2} mb={1} justifyContent="space-between">
-        <Button
-          onClick={() => openDialog(buildIdeaDto({ tabId: props.tabId }))}
-          color="primary"
-          variant="contained"
-        >
-          + New idea (q)
-        </Button>
+        <NewIdeaMenuButton tabId={props.tabId} />
 
         <FlexVCenter gap={2}>
           <FormGroup>
@@ -83,17 +62,6 @@ const GroupTabContent = (props: Props) => {
               label={`Completed ideas`}
             />
           </FormGroup>
-          {/* <FormGroup>
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={!autoScrollIsDisabled}
-                  onClick={() => toggleAutoScroll()}
-                />
-              }
-              label={`Auto-scroll`}
-            />
-          </FormGroup> */}
         </FlexVCenter>
       </FlexVCenter>
     </FlexCol>
---
+++ src/components/GroupPage/GroupTabContent/NewIdeaMenuButton/NewIdeaMenuButton.tsx
@@ -0,0 +1,113 @@
+import SaveCancelButtons from "@/components/_common/buttons/SaveCancelButtons/SaveCancelButtons"
+import FlexCol from "@/components/_common/flexboxes/FlexCol"
+import MyTextField from "@/components/_common/inputs/MyTextField"
+import useSaveIdeaMutation from "@/hooks/react-query/domain/group/tab/idea/useSaveIdeaMutation"
+import useIdeaDialogStore from "@/hooks/zustand/dialogs/useIdeaDialogStore"
+import { buildIdeaDto } from "@/types/domain/group/tab/idea/IdeaDto"
+import { Button, Menu } from "@mui/material"
+import React, { useEffect } from "react"
+import { useHotkeys } from "react-hotkeys-hook"
+
+type Props = {
+  tabId: string
+}
+
+export const NewIdeaMenuButton = ({ ...props }: Props) => {
+  const openDialog = useIdeaDialogStore((s) => s.openDialog)
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const menuIsOpen = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const { mutate: submitNewIdea, isLoading } = useSaveIdeaMutation()
+
+  const buttonRef = React.useRef<HTMLButtonElement>(null)
+  const inputRef = React.useRef<HTMLInputElement>(null)
+
+  const [title, setTitle] = React.useState("")
+  useEffect(() => {
+    if (menuIsOpen) {
+      setTitle("")
+      setTimeout(() => {
+        inputRef.current?.focus()
+      }, 250)
+    }
+  }, [menuIsOpen])
+
+  const handleSave = () => {
+    if (title.trim() === "") {
+      return
+    }
+    submitNewIdea(
+      buildIdeaDto({
+        name: title,
+        tabId: props.tabId,
+      }),
+      {
+        onSuccess: (idea) => {
+          openDialog(idea)
+          handleClose()
+        },
+      }
+    )
+  }
+
+  useHotkeys("q", () => {
+    if (!menuIsOpen) {
+      buttonRef.current?.click()
+    }
+  })
+
+  return (
+    <div>
+      <Button
+        color="primary"
+        variant="contained"
+        ref={buttonRef}
+        onClick={handleClick}
+      >
+        + New idea (q)
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        open={menuIsOpen}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        transformOrigin={{ vertical: "bottom", horizontal: "left" }}
+        sx={{
+          "& .MuiMenu-paper": {
+            minWidth: "300px",
+          },
+        }}
+      >
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            handleSave()
+          }}
+        >
+          <FlexCol gap={1} p={1}>
+            <MyTextField
+              inputRef={inputRef}
+              fullWidth
+              label="Idea title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <SaveCancelButtons
+              disabled={!title.trim()}
+              onCancel={handleClose}
+              isLoadingAndDisabled={isLoading}
+              onEnabledAndCtrlEnter={handleSave}
+            />
+          </FlexCol>
+        </form>
+      </Menu>
+    </div>
+  )
+}
